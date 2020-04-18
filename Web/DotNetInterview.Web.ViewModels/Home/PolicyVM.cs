b6d5c3e4bfc3ef754d4e0aff4387258f1d44d850
Toni <01tonydimitrov@gmail.com>
2020-04-18 21:03:05 +0300

    Add privacy policy

---
+++ Web/DotNetInterview.Web.ViewModels/Home/PolicyVM.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNetInterview.Web.ViewModels.Home
+{
+   public class PolicyVM
+    {
+        public string Content { get; set; }
+    }
+}
--- Web/DotNetInterview.Web/Controllers/HomeController.cs
+++ Web/DotNetInterview.Web/Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿namespace DotNetInterview.Web.Controllers
 {
     using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
 
+    using DotNetInterview.Common;
     using DotNetInterview.Web.ViewModels;
-
+    using DotNetInterview.Web.ViewModels.Home;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
+        private readonly IWebHostEnvironment hostingEnvironment;
+
+        public HomeController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return this.View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
-            return this.View();
+            var imagePath = this.GetRootPath(this.hostingEnvironment, "img\\");
+
+            var filePathAndName = Path.Combine(imagePath, GlobalConstants.PolicyFileName);
+
+            using var reader = new StreamReader(filePathAndName, Encoding.UTF8);
+
+            var policyContent = await reader.ReadToEndAsync();
+
+            return this.View(new PolicyVM { Content = policyContent });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
