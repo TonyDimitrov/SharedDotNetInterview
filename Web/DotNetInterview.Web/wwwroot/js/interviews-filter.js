8719e06d90f9a316a21357b745e9faf3cb988ba4
Vladimir Kraev <vladeto83@gmail.com>
2021-06-17 21:20:47 +0300

    filter-/show-hide-animation/-/left-position/-/background-fix/

---
+++ Web/DotNetInterview.Web/wwwroot/js/interviews-filter.js
@@ -0,0 +1,16 @@
+﻿var div = document.querySelector('#filter-body');
+var icon = document.querySelector('#arrow-icon');
+var filter = document.querySelector('.filter-title')
+var open = false;
+
+filter.addEventListener('click', function () {
+    if (open) {
+        icon.className = 'fa fa-arrow-down';
+        div.className = 'hide';
+    } else {
+        icon.className = 'fa fa-arrow-down open';
+        div.className = '';
+    }
+
+    open = !open;
+});
