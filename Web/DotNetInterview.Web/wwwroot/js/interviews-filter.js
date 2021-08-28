ffde812a6b1d924e215a8cec6484e80e24728234
Vladimir Kraev <vladeto83@gmail.com>
2021-08-28 15:58:52 +0300

    Get query params from filter

--- Web/DotNetInterview.Web/wwwroot/js/interviews-filter.js
+++ Web/DotNetInterview.Web/wwwroot/js/interviews-filter.js
@@ -6,6 +6,12 @@ var iconLegend = document.querySelector('#arrow-icon-legend')
 var divLegend = document.querySelector('#legend-filter-body')
 var open = false;
 
+var findButton = document.querySelector('.find-button')
+
+
+var url = 'https://localhost:5001/Interviews/AllAjax?'
+
+
 filter.addEventListener('click', function () {
     if (open) {
         icon.className = 'fa fa-arrow-down';
@@ -30,3 +36,41 @@ legend.addEventListener('click', function () {
     open = !open;
 });
 
+findButton.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    var nationalityCompany = document.querySelector('.nationality-selection').value
+    var seniorityLevel = document.querySelector('.seniority-selection').value
+    var startDate = document.querySelector('#start-date').value
+    var endDate = document.querySelector('#end-date').value
+
+    console.log(e)
+
+    var finalURL = ''
+    var queryParams = ''
+    var query = {}
+
+    Object.assign(query, { 'nationalityId': nationalityCompany })
+    Object.assign(query, { 'seniority': seniorityLevel })
+    Object.assign(query, { 'from': startDate })
+    Object.assign(query, { 'to': endDate })
+
+    for (let kvp of Object.entries(query)) {
+        if (!kvp[1] == '') {
+            queryParams += (kvp[0] + '=' + kvp[1] + '&')
+        }
+    }
+
+    queryParams = queryParams.slice(0, -1)
+    finalURL = url + queryParams
+    console.log(finalURL)
+
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            const nationalities = response.nationalities.find
+                (m => m.selected === true);
+            console.log(nationalities)
+        })
+})
