8fa85d7df3d9377ce0e695bd69230fab31922ea0
Vladimir Kraev <vladeto83@gmail.com>
2021-09-02 17:41:09 +0300

    Show results from filter

--- Web/DotNetInterview.Web/wwwroot/js/interviews-filter.js
+++ Web/DotNetInterview.Web/wwwroot/js/interviews-filter.js
@@ -8,8 +8,9 @@ var open = false;
 
 var findButton = document.querySelector('.find-button')
 
+var showAllBtn = document.querySelector('.show-all-button')
 
-var url = 'https://localhost:5001/Interviews/AllAjax?'
+var url = '/Interviews/AllAjax?'
 
 
 filter.addEventListener('click', function () {
@@ -44,6 +45,10 @@ findButton.addEventListener('click', function (e) {
     var startDate = document.querySelector('#start-date').value
     var endDate = document.querySelector('#end-date').value
 
+    var interviewsResultBox = document.querySelector('.interviews-result-box')
+    interviewsResultBox.innerHTML = ''
+    let interviewItem = ''
+
     console.log(e)
 
     var finalURL = ''
@@ -69,8 +74,29 @@ findButton.addEventListener('click', function (e) {
         .then(response => response.json())
         .then(response => {
             console.log(response)
-            const nationalities = response.nationalities.find
-                (m => m.selected === true);
-            console.log(nationalities)
+
+            response.interviews.map(item => {
+                interviewItem = `
+                    <div class="row div-row div-question-border-bottom">
+                         <div class="col-10 col-sm-3">${item.positionTitle}</div>
+                         <div class="d-none d-sm-block col-sm-2 seniority-wrapper">
+                              <img class="seniority" src="/img/${item.seniority}">
+                              <span class="seniority-tooltiptext">${item.seniorityTooltip}</span>
+                        </div>
+                        <div class="col-1 col-sm-1"><span class="badge badge-pill span-items-count">${item.questions}</span></div>
+                        <div class="d-none d-sm-block col-sm-2">${item.date}</div>
+                        <div class="d-none d-sm-block col-sm-1"><span class="badge badge-pill span-items-count">${item.likes}</span></div>
+                        <div class="d-none d-sm-block col-sm-2">
+                            <a class="a-user" href="/Users/Details?userId=${item.creatorId}" target="_new">
+                                ${item.creatorName}
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-1 btn-detail">
+                            <a class="btn btn-sm btn-primary" href="/Interviews/Details?interviewId=${item.interviewId}" target="_new">Details</a>
+                        </div>
+                    </div>
+                `
+                interviewsResultBox.innerHTML += interviewItem
+            })
         })
 })
