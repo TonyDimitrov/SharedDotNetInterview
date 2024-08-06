d29ec3a8ab22a27dbbd606c99921d153defbb924
Toni <01tonydimitrov@gmail.com>
2024-08-06 18:21:01 +0300

    Initial blazor test

---
+++ Web/DotNetInterview.Web/Components/CreateInterview.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetInterview.Web.Components
+{
+    using System.Threading.Tasks;
+
+    using DotNetInterview.Web.ViewModels.Interviews;
+    using Microsoft.AspNetCore.Components.Forms;
+
+    public partial class CreateInterview
+    {
+        public CreateInterviewVM CreateInterviewVM { get; set; } = new CreateInterviewVM();
+
+        private async Task HandleFormSubmit(EditContext editContext)
+        {
+            await Task.FromResult(() => {
+                var num = 5 + 5;
+            });
+
+            var jobTitle = this.CreateInterviewVM.PositionTitle;
+        }
+    }
+}
